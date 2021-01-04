30d9f053c56e40136f5b85c865955ca3a53a60ef
Jafar <mendoandev@gmail.com>
2021-01-04 16:22:24 +0700

    populate post stories

--- routes/stories.js
+++ routes/stories.js
@@ -20,11 +20,11 @@ router.get("/", authenticate.verifyUser, (req, res, next) => {
     });
 });
 router.post("/", authenticate.verifyUser, (req, res, next) => {
-  Stories.find({})
+  Stories.find({}).populate("user")
     .then((stories) => {
-        var story = stories.filter(
-            (cek) => cek.user.toString() === req.body.user.toString()
-            )[0];
+        var story = stories.filter( (cek) => cek.user.toString() === req.body.user.toString())[0];
+        // var user = check.filter(cek => cek.user.toString() === req.body.user.toString())[0];
+
       if (!story) {
         story = new Stories({
           user: req.body.user,
