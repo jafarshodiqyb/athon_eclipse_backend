de6149584075d0a5a16cfd41bf06ae2e580d56c7
Jafar <mendoandev@gmail.com>
2021-01-11 21:38:14 +0700

    Wording email

--- helpers/email.js
+++ helpers/email.js
@@ -19,10 +19,10 @@ exports.sendEmail = async (user, host) => {
     await transport.sendMail({
       from: '4b9f2ffd6e-df5375@inbox.mailtrap.io', // sender address
       to: email, // list of receivers
-      subject: 'Hello ✔', // Subject line
+      subject: 'Mini Diarium Email Verification', // Subject line
       text: `
       Hello,
-      thanks for registerin on our site.
+      thanks for registering on our site.
       Please click the link bellow to verify your account.
       ${host}/users/auth/verify-email?token=${verifyToken}`, // plain text body
       html: `<h3>Hello,</h3><p>Thanks for registering on our site</p><p>Please click the link bellow to verify your account.</p><a href='${host}/users/auth/verify-email?token=${verifyToken}'>Verify your account. </a>`, // html body
